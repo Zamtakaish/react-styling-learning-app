366a9e6fac7726654611d2ce7f90a92ef231cd98
Anton Ziuzikov <zuzikov.anton@gmail.com>
2023-10-16 13:55:11 +0300

    feat: use cra inbuilt functionality 'CSS Modules'

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,12 @@
-import styled from "styled-components";
+import React from "react";
 
-import './Button.css';
 
-const Button = styled.button`
+import styles from './Button.module.css';
+
+/*
+  import styled from "styled-components";
+
+  const Button = styled.button`
   width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
@@ -26,6 +30,12 @@ const Button = styled.button`
   border-color: #ac0e77;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
 }
-`;
+`;*/
+
+const Button = props => {
+    return (
+        <button type={props.type} className={styles.button} onClick={props.onClick}>{props.children}</button>
+    )
+}
 
 export default Button;
